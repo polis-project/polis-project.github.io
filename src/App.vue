398bfd4c05d5a7fe8554fe9bda11d80d88900567
qualisign <dvdgdnjsph@gmail.com>
2018-02-04 19:34:13 +0100

    added background pic

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,205 @@
 <template>
     <div id="app">
-        <top-bar></top-bar>
-        <section-pages>
-            <div id="introduction">
-                <top-text>
-                    
-                    
-                </top-text>
-                <div id="use-cases">
-                    <div id="case-internet" class="card">
-                        
-                        <header class="card-header">
-                            <p class="card-header-title">
-                                Component
-                            </p>
-                            <a href="#" class="card-header-icon" aria-label="more options">
-                                <span class="icon">
-                                    <i class="fas fa-angle-down" aria-hidden="true"></i>
-                                </span>
+        <nav class="navbar is-transparent">
+            <div class="navbar-brand">
+                <a class="navbar-item">
+                    <h2>POLIS</h2>
+                </a>
+                <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+
+            <div id="navbarExampleTransparentExample" class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item" href="https://bulma.io/">
+                        Tutorial
+                    </a>
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-link" href="/documentation/overview/start/">
+                            Docs
+                        </a>
+                        <div class="navbar-dropdown is-boxed">
+                            <a class="navbar-item" href="/documentation/overview/start/">
+                                Overview
                             </a>
-                        </header>
-                        <div class="card-content">
-                            <div class="content">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                                <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
-                                <br>
-                                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-                            </div>
+                            <a class="navbar-item" href="https://bulma.io/documentation/modifiers/syntax/">
+                                Modifiers
+                            </a>
+                            <a class="navbar-item" href="https://bulma.io/documentation/columns/basics/">
+                                Columns
+                            </a>
+                            <a class="navbar-item" href="https://bulma.io/documentation/layout/container/">
+                                Layout
+                            </a>
+                            <a class="navbar-item" href="https://bulma.io/documentation/form/general/">
+                                Form
+                            </a>
+                            <hr class="navbar-divider">
+                            <a class="navbar-item" href="https://bulma.io/documentation/elements/box/">
+                                Elements
+                            </a>
+                            <a class="navbar-item is-active" href="https://bulma.io/documentation/components/breadcrumb/">
+                                Components
+                            </a>
                         </div>
-                        <footer class="card-footer">
-                            <a href="#" class="card-footer-item">Save</a>
-                            <a href="#" class="card-footer-item">Edit</a>
-                            <a href="#" class="card-footer-item">Delete</a>
-                        </footer>
-                        
                     </div>
-                    <div id="case-energy"></div>
-                    <div id="case-news"></div>
-                    <div id="case-housing"></div>
-                    <div id="case-rideshare"></div>
-                    <div id="case-builder">                        
-                    </div>                    
-v                </div>
-                <div id="video"></div>
+                    <a class="navbar-item" href="https://bulma.io/">
+                        Protocol
+                    </a>
+                    <a class="navbar-item" href="https://bulma.io/">
+                        ICO
+                    </a>
+                    <a class="navbar-item" href="https://bulma.io/">
+                        Whitepaper
+                    </a>                    
+                </div>
+
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <div class="field is-grouped">
+                            <p class="control">
+                                <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="http://localhost:4000" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=http://localhost:4000&amp;via=jgthms">
+                                    <span class="icon">
+                                        <i class="fab fa-twitter"></i>
+                                    </span>
+                                    <span>
+                                        Tweet
+                                    </span>
+                                </a>
+                            </p>
+                            <p class="control">
+                                <a class="button is-primary" href="https://github.com/jgthms/bulma/archive/0.5.1.zip">
+                                    <span class="icon">
+                                        <i class="fas fa-download"></i>
+                                    </span>
+                                    <span>Download</span>
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>        
+        <section-pages>
+            <div id="introduction"  style="     margin-left: 20px;
+                                           margin-right: 20px;">
+                <section class="hero">
+                    <div class="hero-body">
+                        <div class="container"  style="text-align: center">
+                            <h1 class="title"  style="color: white">
+                                Free, open-source alternatives to sharing economy services
+                            </h1>
+                            <h2 class="subtitle"  style="color: white">
+                                Polis brings together communities of developers, merchants, and customers as co-owners of a universal, decentralized platform business. 
+                            </h2>
+                        </div>
+                    </div>                                        
+                </section>
+                <div class="columns">
+                    <div class="column">
+                        <div class="card">
+                            <div class="card-content">
+                                <p class="title lighter">
+                                    Developers 
+                                </p>
+                                <p class="subtitle">
+                                    Code P2P services.  Get paid by the transaction.  Skip the middleman.  
+                                </p>
+                            </div>
+                            <footer class="card-footer">
+                                <p class="card-footer-item">
+                                    <span>
+                                        View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
+                                    </span>
+                                </p>
+                                <p class="card-footer-item">
+                                    <span>
+                                        Share on <a href="#">Facebook</a>
+                                    </span>
+                                </p>
+                            </footer>                    
+                        </div>
+
+                    </div>
+                    <div class="column">
+                        <div class="card">
+                            <div class="card-content">
+                                <p class="title lighter">
+                                    Merchants
+                                </p>
+                                <p class="subtitle">
+                                    Offer your goods and services.  Keep the lion's share or the profit.
+                                </p>
+                            </div>
+                            <footer class="card-footer">
+                                <p class="card-footer-item">
+                                    <span>
+                                        View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
+                                    </span>
+                                </p>
+                                <p class="card-footer-item">
+                                    <span>
+                                        Share on <a href="#">Facebook</a>
+                                    </span>
+                                </p>
+                            </footer>                    
+                        </div>
+                    </div>
+                    <div class="column">
+
+                        <div class="card">
+                            <div class="card-content">
+                                <p class="title lighter">
+                                    Customers
+                                </p>
+                                <p class="subtitle">
+                                    Find cheaper internet, ridesharing, property rentals, and more.  Retain ownership of your data.
+                                </p>
+                            </div>
+                            <footer class="card-footer">
+                                <p class="card-footer-item">
+                                    <span>
+                                        View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
+                                    </span>
+                                </p>
+                                <p class="card-footer-item">
+                                    <span>
+                                        Share on <a href="#">Facebook</a>
+                                    </span>
+                                </p>
+                            </footer>                    
+                        </div>        
+                    </div>
+                    <div class="column">
+
+                        <div class="card">
+                            <div class="card-content">
+                                <p class="title lighter">
+                                    Investors
+                                </p>
+                                <p class="subtitle">
+                                    Invest in a coin founded on utility.
+                                    
+                                </p>
+                            </div>
+                            <footer class="card-footer">
+                                <p class="card-footer-item">
+                                    <span>
+                                        View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
+                                    </span>
+                                </p>
+                                <p class="card-footer-item">
+                                    <span>
+                                        Share on <a href="#">Facebook</a>
+                                    </span>
+                                </p>
+                            </footer>                    
+                        </div>                                
+                    </div>
+                </div>                                        
             </div>
             <div id="roles">
                 
@@ -70,10 +227,22 @@ v                </div>
                     <div id="news"></div>
                     <div id="real estate"></div>
                     <div id="insurance"></div>
-                </div>            
-        </section-pages>
-
+                </div>
             </div>
+        </section-pages>
+        <footer class="footer" style="bottom: 0px">
+            <div class="container">
+                <div class="content has-text-centered">
+                    <p>
+                        <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+                        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+                        is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+                    </p>
+                </div>
+            </div>
+        </footer>
+        
+    </div>
 </template>
 
 <script>
@@ -90,16 +259,18 @@ v                </div>
 
 <style>
  #app {
+     background-image: url("../static/caryatids.jpg");
      font-family: 'Avenir', Helvetica, Arial, sans-serif;
      -webkit-font-smoothing: antialiased;
      -moz-osx-font-smoothing: grayscale;
-     text-align: center;
-     color: #2c3e50;
-     margin-top: 60px;
+     background-size:100%;
  }
 
  h1, h2 {
      font-weight: normal;
+ }
+ h2 {
+     font-size: 25px;
  }
 
  ul {
@@ -115,4 +286,20 @@ v                </div>
  a {
      color: #42b983;
  }
+ .card {
+     width: 100%;
+     height: 100%;
+     display: flex;
+     flex-direction: column;
+     background-color: rgba(255, 255, 255, 0.9);
+}
+ .card-footer {
+     margin-top: auto;
+ }
+ 
+ .lighter {    
+     color: #42b983;
+     font-weight: lighter;
+ }
+ 
 </style>
